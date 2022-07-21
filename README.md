5dbf93a0a84579a4fd8a0eb12cebd658e33ede68
John Sattari <johnsattari@johns-air.lan>
2022-07-20 22:27:40 -0700

    Updated code cleanup: Added new instructions; updated notes

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Port = 5000
 **INSTALL COMMANDS**
 1. Create a virtual environment and install requirements (`pip3 install -r requirements.txt`)
 2. `deactivate` your virtual environment and navigate to the root project folder
-3. Run `./bootstrap.sh` command from the root directory
+3. Run `./bootstrap.sh &` command from the root directory
 4. Once the flask server is running, enter curl commands in the following format to access API (ex.: `curl http://localhost:5000/api/posts&tags=history,tech&sortBy=likes&sortOrder=desc"`)
--- api/main.py
+++ api/main.py
@@ -8,19 +8,29 @@ from api.tools.spool import *
 
 app = Flask(__name__)
 
+# lock for threads
 lock = Lock()
 
-
+# ping route, only returns success or fail
 @app.route("/ping", methods=["GET"])
 def ping():
     return jsonify({"success": True}), 200
 
 
+# posts route, gets data for api request
 @app.route("/posts", methods=["GET", "POST"])
 def get_posts():
 
+    """
+    Accepts query string params to make a request from blog post
+    API endpoints. Creates multiple threads to make multiple API
+    requests as fast as possible. Includes sorting where applicable.
+    """
+
+    # get query strings params from url
     data = request.args
 
+    # create variables for holding params
     tags = [
         f"https://api.hatchways.io/assessment/blog/posts?tag={tag}"
         for tag in data.get("tag").split(",")
@@ -29,16 +39,22 @@ def get_posts():
     sortOrder = data.get("direction")
 
     try:
+
+        # list to hold api results
         api_results = []
 
+        # list to hold threads
         threads = []
 
+        # enumerate through tags, create thread for each tag and add to api_results
         for key, value in enumerate(tags):
             threads.append(Worker(key, make_request, value, api_results))
 
+        # join each thread in threads, this allows all threads to finish
         for thread in threads:
             thread.join()
 
+        # sort and de-duplicate api_results list
         api_results = sort_array(get_uniques(api_results), sortBy, sortOrder)
 
         return jsonify({"posts": api_results}), 200
